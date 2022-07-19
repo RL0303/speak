41d250ab41955735cc4c339a89627abd93e9c77a
Robert <linrobert0303@gmail.com>
2022-07-19 22:00:26 +0800

    Initial Commit

---
+++ speak/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  speak
+//
+//  Created by 林辰澤 on 2022/7/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
