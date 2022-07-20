243bb8a281bc61908c03dec1cf174d40f0a80f07
Robert <linrobert0303@gmail.com>
2022-07-20 17:08:58 +0800

    speak app

--- speak/ViewController.swift
+++ speak/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loveTextField: UITextField!
+    
+    @IBOutlet weak var rateSlider: UISlider!
+    
+    @IBOutlet weak var pitchMultiplierSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func speak(_ sender: Any) {
+        print(loveTextField.text!)
+        
+        let speechUtterance =  AVSpeechUtterance(string: loveTextField.text!)
+        //設定語言voice為中文代碼
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        //語速rate 0~1
+        speechUtterance.rate = rateSlider.value
+        //音調pitchMultiplier 0.5~2
+        speechUtterance.pitchMultiplier = pitchMultiplierSlider.value
+        //音量volume 0~1
+        speechUtterance.volume = 1
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(speechUtterance)
+    }
+    
+    
+    @IBAction func defaultSpeak(_ sender: Any) {
+        print("如果海浪是大海的事，那麼想你就是我的事。")
+        
+        let speechUtterance =  AVSpeechUtterance(string: "如果海浪是大海的事，那麼想你就是我的事。")
+        //設定語言voice為中文代碼
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        //語速rate 0~1
+        speechUtterance.rate = 0.4
+        //音調pitchMultiplier 0.5~2
+        speechUtterance.pitchMultiplier = 0.9
+        //音量volume 0~1
+        speechUtterance.volume = 1
+        let synthesizer = AVSpeechSynthesizer()
+        synthesizer.speak(speechUtterance)
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+//    按下return收起鍵盤
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       self.view.endEditing(true)
+       return true
+    }
+}
